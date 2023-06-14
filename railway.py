8934f83fb77163dfcf82b386cd4d8c830d8435d3
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-06-14 15:28:58 +0700

    first commit

---
+++ railway.py
@@ -0,0 +1,39 @@
+import psycopg2
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+def get_all_users():
+    conn = psycopg2.connect(
+        host="containers-us-west-186.railway.app",
+        port="7559",
+        database="railway",
+        user="postgres",
+        password="ks492yYQCJFTYQuSe1u2"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users;")
+    users = cursor.fetchall()
+    conn.close()
+    return users
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = get_all_users()
+    response = []
+    for user in users:
+        user_data = {
+            'id': user[0],
+            'name': user[1],
+            'role': user[2],
+        }
+        response.append(user_data)
+    return {
+        'data': response,
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
